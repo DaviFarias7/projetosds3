1dd78e98d2a2b4f2bec04e3032d97316970a575d
DaviFarias7 <davic.farias7@gmail.com>
2021-11-06 22:29:39 -0300

    error Barchart:Netlify

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
+import { SaleSuccess } from 'types/sale';
 import { round } from 'utils/format';
 import { BASE_URL } from 'utils/requests';
 
