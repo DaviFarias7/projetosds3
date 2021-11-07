7523b3fc2aa8a7fbe257e60691bbf2752c7fb44c
DaviFarias7 <davic.farias7@gmail.com>
2021-11-06 22:23:39 -0300

    error Barchart: deleted import { SaleSuccess, SaleSum } from 'types/sale';

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
-import { SaleSuccess, SaleSum } from 'types/sale';
 import { round } from 'utils/format';
 import { BASE_URL } from 'utils/requests';
 
